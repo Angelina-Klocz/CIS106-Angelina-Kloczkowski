097f314d6ae0d87c292ef3245f1bf9a1cf874d1d
Angelina-Klocz <angelina.kloczkowski@yahoo.com>
2026-03-01 16:58:43 -0600

    Add files via upload

---
+++ PS3P5.py
@@ -0,0 +1,9 @@
+#PS3P5: Circle Area and Perimeter
+radius = float(input("Enter the radius of the circle: "))
+pi=3.174
+#Process
+area = pi * radius ** 2
+perimeter = 2 * pi * radius
+#Output
+print("Area of the circle:", area)
+print("Perimeter of the circle:", perimeter)
