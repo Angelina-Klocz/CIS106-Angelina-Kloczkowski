991d97d297cb08f4dd3d82d01786063a1718aa05
Angelina-Klocz <angelina.kloczkowski@yahoo.com>
2026-03-01 16:00:23 -0600

    Delete PS3P2.py

--- PS3P2.py
+++
@@ -1,9 +0,0 @@
-#PS3P2: Student Exam Total
-last_name= input("Enter your last name: ")
-midterm= float(input("Enter your midterm score: "))
-final= float(input("Enter your final score: "))
-#Process
-total_points= midterm + final
-#Output
-print("Name: ", last_name)
-print("total_points: ", total_points)
