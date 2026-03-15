5a08be9e7b81e12ca068c5828360a0402d357753
Angelina-Klocz <angelina.kloczkowski@yahoo.com>
2026-03-15 16:16:10 -0500

    Add files via upload

---
+++ PS5P5.py
@@ -0,0 +1,12 @@
+name = input("Enter employee last name: ")
+salary = float(input("Enter salary: "))
+level = int(input("Enter job level: "))
+if level >= 10:
+  rate = 0.25
+elif level >= 5:
+  rate = 0.20
+else:
+  rate = 0.10
+bonus = salary * rate
+print("Employee name:", name)
+print("Bonus: $", bonus)
